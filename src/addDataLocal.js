5ae37d66acf2822341c8a6f648657d3ed7b328de
AhemdAboAbadi <ahmed.free.py@gmail.com>
2021-09-22 18:21:52 +0300

    set data to local storage and add router

---
+++ src/addDataLocal.js
@@ -0,0 +1,24 @@
+const addDataLocal = (id) => {
+  let dataLocalStorage = []
+
+  try {
+      dataLocalStorage = JSON.parse(localStorage['favorite-film'])
+  } catch (error) {
+      dataLocalStorage = 'erorr'
+  }
+
+  console.log(dataLocalStorage);
+
+  
+  if (typeof dataLocalStorage != 'object'){
+      localStorage.setItem('favorite-film',JSON.stringify([id]))
+      console.log(JSON.parse(localStorage['favorite-film']));
+  }else {
+      dataLocalStorage.push(id)
+      localStorage.setItem('favorite-film',JSON.stringify(dataLocalStorage))
+  }   
+}
+  
+
+
+export default addDataLocal;
