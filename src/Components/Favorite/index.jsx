b6a42394327aa9f17d1be1820b71e41f50b24d67
AhemdAboAbadi <ahmed.free.py@gmail.com>
2021-09-22 15:11:31 +0300

    file Structure

---
+++ src/Components/Favorite/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './style.css';
+
+import { BrowserRouter as Router, Switch, Link } from 'react-router-dom';
+
+
+function Favorite({setSearchValueInput}) {
+
+
+
+
+
+  return (
+    <div>
+      hi Favorite
+    </div>
+  );
+}
+
+export default Favorite;
+
+
+
+
